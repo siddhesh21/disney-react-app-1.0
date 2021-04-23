c6b158e8a00f5dfa2e31a202d886355f09658845
Siddhesh Kankekar <kankekas@tcd.ie>
2021-04-23 20:38:44 +0100

    Added images+videos + bg-img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
